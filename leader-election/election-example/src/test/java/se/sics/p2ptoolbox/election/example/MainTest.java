0a5a8cb760193abd1cb73489653d014432cecd63
Abhimanyu Babbar <babbar@kth.se>
2015-04-16 20:21:36 +0200

    Updated the test class to set the client address in the system.

--- leader-election/election-example/src/test/java/se/sics/p2ptoolbox/election/example/MainTest.java
+++ leader-election/election-example/src/test/java/se/sics/p2ptoolbox/election/example/MainTest.java
@@ -2,12 +2,20 @@ package se.sics.p2ptoolbox.election.example;
 
 import se.sics.gvod.config.VodConfig;
 import se.sics.kompics.Kompics;
+import se.sics.kompics.network.netty.serialization.Serializer;
+import se.sics.kompics.network.netty.serialization.Serializers;
 import se.sics.kompics.simulation.SimulatorScheduler;
+import se.sics.p2ptoolbox.election.example.main.LEDescriptorSerializer;
+import se.sics.p2ptoolbox.election.example.main.LeaderDescriptor;
 import se.sics.p2ptoolbox.election.example.scenario.LeaderElectionScenario;
 import se.sics.p2ptoolbox.election.network.ElectionSerializerSetup;
 import se.sics.p2ptoolbox.simulator.run.LauncherComp;
+import se.sics.p2ptoolbox.util.network.impl.BasicAddress;
+import se.sics.p2ptoolbox.util.network.impl.DecoratedAddress;
+import se.sics.p2ptoolbox.util.serializer.BasicSerializerSetup;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 /**
  * Main Test Class for the Leader Election Protocol.
@@ -17,14 +25,29 @@ import java.io.IOException;
 public class MainTest {
 
     public static long seed = 123;
+    public static String descriptorName = "leDescriptor";
 
     public static void main(String[] args) throws IOException {
 
         VodConfig.init(new String[]{});
-        System.out.println(" Serialization SetUp Successful ");
+        System.out.println("Starting the serializers registration.");
+        int startId=0;
 
-        ElectionSerializerSetup.registerSerializers(0);
+        BasicSerializerSetup.registerBasicSerializers(startId);
+        startId += BasicSerializerSetup.serializerIds;
 
+        startId = ElectionSerializerSetup.registerSerializers(startId);
+        ElectionSerializerSetup.checkSetup();
+
+        Serializer leaderDescriptorSerializer = new LEDescriptorSerializer(startId);
+        Serializers.register(leaderDescriptorSerializer, descriptorName);
+        Serializers.register(LeaderDescriptor.class, descriptorName);
+
+        System.out.println("Final serializer registration complete.");
+
+        System.out.println("Setting the client address.");
+
+        LauncherComp.simulatorClientAddress = new DecoratedAddress(new BasicAddress(InetAddress.getByName("127.0.0.1"), 30000, -1));
         LauncherComp.scheduler = new SimulatorScheduler();
         LauncherComp.scenario = LeaderElectionScenario.boot(seed);
 
