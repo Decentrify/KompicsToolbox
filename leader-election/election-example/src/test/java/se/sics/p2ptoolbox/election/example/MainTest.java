4c66400b72be875cb446b68af61f267719a430c2
Abhimanyu Babbar <babbar@kth.se>
2015-04-16 19:53:39 +0200

    Updated the main test file and gitignore file.

--- leader-election/election-example/src/test/java/se/sics/p2ptoolbox/election/example/MainTest.java
+++ leader-election/election-example/src/test/java/se/sics/p2ptoolbox/election/example/MainTest.java
@@ -1,5 +1,12 @@
 package se.sics.p2ptoolbox.election.example;
 
+import se.sics.gvod.config.VodConfig;
+import se.sics.kompics.Kompics;
+import se.sics.kompics.simulation.SimulatorScheduler;
+import se.sics.p2ptoolbox.election.example.scenario.LeaderElectionScenario;
+import se.sics.p2ptoolbox.election.network.ElectionSerializerSetup;
+import se.sics.p2ptoolbox.simulator.run.LauncherComp;
+
 import java.io.IOException;
 
 /**
@@ -13,6 +20,21 @@ public class MainTest {
 
     public static void main(String[] args) throws IOException {
 
+        VodConfig.init(new String[]{});
+        System.out.println(" Serialization SetUp Successful ");
+
+        ElectionSerializerSetup.registerSerializers(0);
+
+        LauncherComp.scheduler = new SimulatorScheduler();
+        LauncherComp.scenario = LeaderElectionScenario.boot(seed);
+
+        Kompics.setScheduler(LauncherComp.scheduler);
+        Kompics.createAndStart(LauncherComp.class, 1);
+        try {
+            Kompics.waitForTermination();
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
 
     }
 
