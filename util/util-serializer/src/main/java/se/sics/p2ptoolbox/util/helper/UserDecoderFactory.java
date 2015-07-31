646d7902f85e3b218ea96b186a6dacbcbc48b1ab
Abhimanyu Babbar <abhimanyu.babbar89@gmail.com>
2015-07-31 14:23:55 +0200

    Added more helper methods to the encoder / decoder factories.

--- util/util-serializer/src/main/java/se/sics/p2ptoolbox/util/helper/UserDecoderFactory.java
+++ util/util-serializer/src/main/java/se/sics/p2ptoolbox/util/helper/UserDecoderFactory.java
@@ -1,10 +1,13 @@
 package se.sics.p2ptoolbox.util.helper;
 
+import com.google.common.base.Optional;
 import io.netty.buffer.ByteBuf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.sics.kompics.network.netty.serialization.Serializer;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Collection;
 
 /**
  * Main factory containing helper functions for
@@ -25,9 +28,7 @@ public class UserDecoderFactory {
         }
     }
 
-
-
-    public static int readUnsignedIntAsTwoBytes(ByteBuf buffer) //            throws MessageDecodingException
+    public static int readUnsignedIntAsTwoBytes(ByteBuf buffer)
     {
         byte[] bytes = new byte[2];
         buffer.readBytes(bytes);
@@ -49,4 +50,35 @@ public class UserDecoderFactory {
         }
     }
 
+    public static String readStringLength256(ByteBuf buffer) throws DecodingException{
+
+        int len = readIntAsOneByte(buffer);
+        return len == 0?null:readString(buffer, len);
+    }
+
+    public static int readIntAsOneByte(ByteBuf buffer) {
+        return readUnsignedIntAsOneByte(buffer);
+    }
+
+    public static int readUnsignedIntAsOneByte(ByteBuf buffer) {
+        byte value = buffer.readByte();
+        return value & 255;
+    }
+
+    /**
+     * Read the collection from the buffer.
+     *
+     * @param objCollection objCollection
+     * @param buffer buffer
+     */
+    public static void readCollectionFromBuff(Collection objCollection, Serializer serializer, ByteBuf buffer, Optional<Object> hint){
+
+        int size = buffer.readInt();
+
+        while(size > 0) {
+            Object entry = serializer.fromBinary(buffer, hint);
+            objCollection.add(entry);
+            size--;
+        }
+    }
 }
--- util/util-serializer/src/main/java/se/sics/p2ptoolbox/util/helper/UserEncoderFactory.java
+++ util/util-serializer/src/main/java/se/sics/p2ptoolbox/util/helper/UserEncoderFactory.java
@@ -1,10 +1,13 @@
 package se.sics.p2ptoolbox.util.helper;
 
+import com.google.common.io.BaseEncoding;
 import io.netty.buffer.ByteBuf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.sics.kompics.network.netty.serialization.Serializer;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Collection;
 
 /**
  * Factory containing various helper methods for serializing
@@ -16,6 +19,77 @@ public class UserEncoderFactory {
 
     private static Logger logger = LoggerFactory.getLogger(UserEncoderFactory.class);
 
+
+
+
+    public static void writeStringLength256(ByteBuf buffer, String str) throws EncodingException {
+        if(str == null) {
+            writeUnsignedintAsOneByte(buffer, 0);
+        } else {
+            if(str.length() > 255) {
+                throw new EncodingException("String length > 255 : " + str);
+            }
+
+            byte[] strBytes;
+            try {
+                strBytes = str.getBytes("UTF-8");
+            } catch (UnsupportedEncodingException var4) {
+                throw new EncodingException("Unsupported chartset when encoding string: UTF-8");
+            }
+
+            int len = strBytes.length;
+            writeUnsignedintAsOneByte(buffer, len);
+            buffer.writeBytes(strBytes);
+        }
+
+    }
+
+
+    public static void writeUnsignedintAsOneByte(ByteBuf buffer, int value) throws EncodingException {
+        if((double)value < Math.pow(2.0D, 8.0D) && value >= 0) {
+            buffer.writeByte((byte)(value & 255));
+        } else {
+            throw new EncodingException("writeUnsignedintAsOneByte: Integer value < 0 or " + value + " is larger than 2^15");
+        }
+    }
+
+
+    /**
+     * Simple helper method to write a collection
+     * to byte buffer.
+     *
+     * @param objCollection
+     * @param serializer
+     * @param buf
+     */
+    public static void collectionToBuff(Collection objCollection, Serializer serializer, ByteBuf buf){
+
+        int size = objCollection.size();
+        buf.writeInt(size);
+
+        for(Object obj : objCollection){
+            serializer.toBinary(obj, buf);
+        }
+    }
+
+
+    /**
+     * In cases where the object could be null, extra information
+     * needs to be placed before writing the object.
+     *
+     * @param buf buffer
+     * @param obj object
+     */
+    public static void checkNullAndUpdateBuff(ByteBuf buf, Object obj){
+
+        if(obj == null){
+            buf.writeBoolean(true);
+        }
+        else{
+            buf.writeBoolean(false);
+        }
+
+    }
 
 
 
