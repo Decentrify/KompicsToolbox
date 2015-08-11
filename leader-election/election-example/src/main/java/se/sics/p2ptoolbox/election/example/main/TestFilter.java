9756d6a79a0041352f8e59e0726a28388c86cb67
Abhimanyu Babbar <abhimanyu.babbar89@gmail.com>
2015-08-11 13:47:51 +0200

    Added application based response for the initiating and extension of leadership.

--- leader-election/election-example/src/main/java/se/sics/p2ptoolbox/election/example/main/TestFilter.java
+++ leader-election/election-example/src/main/java/se/sics/p2ptoolbox/election/example/main/TestFilter.java
@@ -2,6 +2,9 @@ package se.sics.p2ptoolbox.election.example.main;
 
 import se.sics.p2ptoolbox.election.api.LCPeerView;
 import se.sics.p2ptoolbox.election.core.util.LeaderFilter;
+import se.sics.p2ptoolbox.util.network.impl.DecoratedAddress;
+
+import java.util.Collection;
 
 /**
  * Test Filter for the application.
@@ -9,6 +12,11 @@ import se.sics.p2ptoolbox.election.core.util.LeaderFilter;
  * Created by babbar on 2015-04-04.
  */
 public class TestFilter implements LeaderFilter{
+
+    @Override
+    public boolean initiateLeadership(Collection<DecoratedAddress> cohorts) {
+        return true;
+    }
 
     @Override
     public boolean terminateLeader(LCPeerView old, LCPeerView updated) {
