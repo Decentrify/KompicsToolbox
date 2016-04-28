c55e60652b49e3df36cc023e0047e70e54d11390
Alex Ormenisan <ormenisan.adrian@gmail.com>
2016-04-28 09:03:25 +0200

    init hops storage commit

---
+++ hops/src/main/java/se/sics/ktoolbox/hops/managedStore/storage/HopsDataStorage.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2009 Swedish Institute of Computer Science (SICS) Copyright (C)
+ * 2009 Royal Institute of Technology (KTH)
+ *
+ * KompicsToolbox is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package se.sics.ktoolbox.hops.managedStore.storage;
+
+import se.sics.ktoolbox.util.managedStore.core.Storage;
+
+/**
+ * @author Alex Ormenisan <aaor@kth.se>
+ */
+public class HopsDataStorage implements Storage {
+
+    @Override
+    public byte[] read(long readPos, int readLength) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int write(long writePos, byte[] bytes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long length() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
