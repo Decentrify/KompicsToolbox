51e4ca7cfd72d2879606380ce9787fd0a796c8d3
Alex Ormenisan <ormenisan.adrian@gmail.com>
2016-06-15 11:42:18 +0200

    added user to create/delete files

--- hops/src/main/java/se/sics/ktoolbox/hops/managedStore/storage/HDFSHelper.java
+++ hops/src/main/java/se/sics/ktoolbox/hops/managedStore/storage/HDFSHelper.java
@@ -19,6 +19,7 @@
 package se.sics.ktoolbox.hops.managedStore.storage;
 
 import java.io.IOException;
+import java.security.PrivilegedExceptionAction;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,12 +29,14 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FsStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.slf4j.LoggerFactory;
 
 /**
  * @author Alex Ormenisan <aaor@kth.se>
  */
 public class HDFSHelper {
+
     private final static org.slf4j.Logger LOG = LoggerFactory.getLogger(HDFSHelper.class);
     private static String logPrefix = "";
 
@@ -53,49 +56,72 @@ public class HDFSHelper {
         }
     }
 
-    public static boolean delete(String hopsIp, int hopsPort, String dirPath, String fileName) {
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", hopsIp + ":" + hopsPort);
-        try (DistributedFileSystem fs = (DistributedFileSystem) FileSystem.get(conf)) {
-            fs.delete(new Path(dirPath + Path.SEPARATOR + fileName), false);
-            return true;
-        } catch (IOException ex) {
+    public static boolean delete(final String user, final String hopsIp, final int hopsPort, final String dirPath, final String fileName) {
+        UserGroupInformation ugi = UserGroupInformation.createRemoteUser(user);
+        try {
+            boolean result = ugi.doAs(new PrivilegedExceptionAction<Boolean>() {
+                public Boolean run() throws Exception {
+                    final Configuration conf = new Configuration();
+                    conf.set("fs.defaultFS", hopsIp + ":" + hopsPort);
+                    conf.set("hadoop.job.ugi", user);
+                    try (FileSystem fs = FileSystem.get(conf)) {
+                        fs.delete(new Path(dirPath + Path.SEPARATOR + fileName), false);
+                        return true;
+                    } catch (IOException ex) {
+                        LOG.warn("{}could not delete file:{}", logPrefix, ex.getMessage());
+                        return false;
+                    }
+                }
+            });
+            return result;
+        } catch (IOException | InterruptedException ex) {
             LOG.warn("{}could not delete file:{}", logPrefix, ex.getMessage());
             return false;
         }
     }
 
-    public static boolean create(String hopsIp, int hopsPort, String dirPath, String fileName, long fileSize) {
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", hopsIp + ":" + hopsPort);
-        try (DistributedFileSystem fs = (DistributedFileSystem) FileSystem.get(conf)) {
-            if (!fs.isDirectory(new Path(dirPath))) {
-                fs.mkdirs(new Path(dirPath));
-            }
-            String filePath = dirPath + Path.SEPARATOR + fileName;
-            if (fs.isFile(new Path(filePath))) {
-                return false;
-            }
-            Random rand = new Random(1234);
-            try (FSDataOutputStream out = fs.create(new Path(filePath))) {
-                for (int i = 0; i < fileSize / 1024; i++) {
-                    byte[] data = new byte[1024];
-                    rand.nextBytes(data);
-                    out.write(data);
-                    out.flush();
+    public static boolean create(final String user, final String hopsIp, final int hopsPort, final String dirPath, final String fileName, final long fileSize) {
+        UserGroupInformation ugi = UserGroupInformation.createRemoteUser(user);
+        try {
+            boolean result = ugi.doAs(new PrivilegedExceptionAction<Boolean>() {
+                public Boolean run() throws Exception {
+                    Configuration conf = new Configuration();
+                    conf.set("fs.defaultFS", hopsIp + ":" + hopsPort);
+                    try (FileSystem fs = FileSystem.get(conf)) {
+                        if (!fs.isDirectory(new Path(dirPath))) {
+                            fs.mkdirs(new Path(dirPath));
+                        }
+                        String filePath = dirPath + Path.SEPARATOR + fileName;
+                        if (fs.isFile(new Path(filePath))) {
+                            return false;
+                        }
+                        Random rand = new Random(1234);
+                        try (FSDataOutputStream out = fs.create(new Path(filePath))) {
+                            for (int i = 0; i < fileSize / 1024; i++) {
+                                byte[] data = new byte[1024];
+                                rand.nextBytes(data);
+                                out.write(data);
+                                out.flush();
+                            }
+                            if (fileSize % 1024 != 0) {
+                                byte[] data = new byte[(int) (fileSize % 1024)];
+                                rand.nextBytes(data);
+                                out.write(data);
+                                out.flush();
+                            }
+                            return true;
+                        } catch (IOException ex) {
+                            LOG.warn("{}could not write file:{}", logPrefix, ex.getMessage());
+                            return false;
+                        }
+                    } catch (IOException ex) {
+                        LOG.warn("{}could not create file:{}", logPrefix, ex.getMessage());
+                        return false;
+                    }
                 }
-                if(fileSize % 1024 != 0) {
-                    byte[] data = new byte[(int)(fileSize % 1024)];
-                    rand.nextBytes(data);
-                    out.write(data);
-                    out.flush();
-                }
-                return true;
-            } catch (IOException ex) {
-                LOG.warn("{}could not create file:{}", logPrefix, ex.getMessage());
-                return false;
-            }
-        } catch (IOException ex) {
+            });
+            return result;
+        } catch (IOException | InterruptedException ex) {
             LOG.warn("{}could not create file:{}", logPrefix, ex.getMessage());
             return false;
         }
